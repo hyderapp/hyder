79d6cc76ea707b37c1ee9c945e64163de9d528e5
bencode <bencode@163.com>
2019-01-30 14:35:21 +0800

    init examples

---
+++ packages/examples/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+
+
+const App = () => {
+  return (
+    <div>Hello</div>
+  );
+};
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
