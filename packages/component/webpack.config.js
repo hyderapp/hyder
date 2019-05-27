948640357c52132d52849be56fe6637237955f46
bencode <bencode@163.com>
2019-05-27 15:38:06 +0800

    update deps libs

---
+++ packages/component/webpack.config.js
@@ -0,0 +1,42 @@
+const pathUtil = require('path');
+
+const reactExternal = {
+  root: 'React',
+  commonjs2: 'react',
+  commonjs: 'react',
+  amd: 'react'
+};
+
+const reactDOMExternal = {
+  root: 'ReactDOM',
+  commonjs2: 'react-dom',
+  commonjs: 'react-dom',
+  amd: 'react-dom'
+};
+
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+
+  externals: {
+    react: reactExternal,
+    'react-dom': reactDOMExternal
+  },
+
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js',
+    path: pathUtil.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    libraryTarget: 'umd',
+    library: 'HyderComponent'
+  },
+
+  module: {
+    rules: [
+      { test: /\.js?$/, use: { loader: 'babel-loader' } }
+    ]
+  }
+};
